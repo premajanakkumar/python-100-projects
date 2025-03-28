f5ac5fedeb6eb41cd382deaf4661b4b5e606a830
PREMA JANAKKUMAR <prema.janakkumar@kyndryl.com>
2025-03-28 12:28:25 +0530

    adding new files

--- Project-1-AddDate-to-files/mtest.py
+++ Project-1-AddDate-to-files/mtest.py
@@ -1,18 +1,16 @@
 import os
 from datetime import datetime
 
-
 directory = 'files'
 filenames = os.listdir(directory)
-print(filenames)
-for filename in filenames:
+for filename in filenames: 
     filepath = os.path.join(directory,filename)
-    day = datetime.now().strftime("%A")
     with open(filepath,'r') as file:
         content = file.read()
         words = content.split()
-        words_count = len(words)
-        print(words_count)
-    new_file_name = f'{filename[:-4]}-{words_count}-{day}.txt'
+        word_count = len(words)
+        day = datetime.now().strftime('%A')
+        
+    new_file_name = f'{filename[:1]}.txt'
     new_file_path = os.path.join(directory,new_file_name)
-    os.rename(filepath,new_file_path)
+    os.rename(filepath,new_file_path) 
